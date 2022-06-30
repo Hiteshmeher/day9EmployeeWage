f54020b51e46a8c6ad7fae5ad2ae667a0a1ead29
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-30 17:06:10 +0530

    using class method

---
+++ src/day9EmployeeWage/EmpW.java
@@ -0,0 +1,11 @@
+package day9EmployeeWage;
+
+public class EmpW {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Problem ");
+		EmployeeWage emp = new EmployeeWage();
+		emp.Emp();
+	}
+
+}
