180a1fab8f76c50d3e0065936e295c3b00bbf6cf
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-30 16:46:52 +0530

    calculating part time employee wages

--- src/day9EmployeeWage/EmployeeWage.java
+++ src/day9EmployeeWage/EmployeeWage.java
@@ -2,15 +2,17 @@ package day9EmployeeWage;
 
 public class EmployeeWage {
 
-	static final int WagePerHour = 20;
-	static final int FullDayHours = 8;
+	static int WagePerHour = 20;
+	static int FullDayHour = 8;
+	static int PartTimeHour = 4;
 
 	public static void main(String[] args) {
 		/**
 		 * generating random number 0, 1, 2 
-		 * if o then Employee is Absent
-		 * if 1 then present
-		 * if 2 then Employee is doing Half day
+		 * if o then Employee is Absent 
+		 * if 1 then Employee Present and claculating wages 
+		 * if 2 then Employee is doing Half day and calculating wages
+		 * 
 		 */
 
 		System.out.println("Welcome to Employee Wage Problem::");
@@ -20,10 +22,12 @@ public class EmployeeWage {
 			System.out.println("Emloyee is absent");
 		else if (attendance == 1) {
 			System.out.println("Employee is present");
-			dailyWages = WagePerHour * FullDayHours;
-			System.out.println("Daily Wages of Employee: " + dailyWages);
+			dailyWages = WagePerHour * FullDayHour;
+			System.out.println("Wages of the day is  " + dailyWages);
 		} else {
-		            System.out.println("Employee is present for the Half day");
-		        }
-		    }
+			System.out.println("Employee is present for the Half day");
+			dailyWages = WagePerHour * PartTimeHour;
+			System.out.println("Wages of the day for part time " + dailyWages);
 		}
+	}
+}
